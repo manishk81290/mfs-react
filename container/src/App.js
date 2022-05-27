dbaa5d207eccb06068332e3d768d0a51a069870c
Manish Kumar <mankumar97@WKMZT077C4EA.global.publicisgroupe.net>
2022-05-27 14:21:06 +0530

    generate classname added for container mf

--- container/src/App.js
+++ container/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Header from './components/Header';
 import MFMarketing from './components/MFMarketing';
 
+const generateClassName=createGenerateClassName({
+    productionPrefix: 'mf-con',
+});
+
 export default ()=>{
-    return <BrowserRouter>
-        <Header />
-        <MFMarketing />
-    </BrowserRouter>
+    return <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+            <Header />
+            <MFMarketing />
+        </BrowserRouter>
+    </StylesProvider>
 };
